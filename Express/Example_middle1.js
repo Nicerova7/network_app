832198e5cc48f347c23470005c1cbc2c5409a929
Nicerova7 <nil_al_7@hotmail.com>
2017-09-27 13:18:19 -0500

    Example middleware functions

---
+++ Express/Example_middle1.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var port = 8000;
+
+app.get('/',log,hello);
+
+function log(req, res, next){
+	console.log(new Date(), req.method, req.url);
+	next();
+}
+
+function hello(req, res, next){
+	res.write('Hello \n' + 'World');
+	res.end();
+	next();
+}
+
+app.listen(port,function(){
+	console.log('server started on port 8k');
+});
